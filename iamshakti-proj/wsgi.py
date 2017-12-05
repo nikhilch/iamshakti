011e82de7eba1f6d17cff4d1173b63673470c30b
Nikhil <nikhofthehil@gmail.com>
2017-12-04 21:16:37 -0600

    Fixing name change

--- iamshakti-proj/wsgi.py
+++ iamshakti-proj/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iamshakti.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iamshakti-proj.settings")
 
 application = get_wsgi_application()
