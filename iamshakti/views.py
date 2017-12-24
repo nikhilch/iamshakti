1959418ed57c3efab7aeb7694dce2bdca9345d9b
Nikhil <nikhofthehil@gmail.com>
2017-12-24 14:59:37 -0600

    Adding test template

--- iamshakti/views.py
+++ iamshakti/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Welcome to the index (or should I say WINdex ayyyy)")
+    template = loader.get_template('base.html')
+    context = { 'yolo': 3, }
+    return HttpResponse(template.render(context, request))
