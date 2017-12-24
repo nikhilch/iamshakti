e48263655ca0bbdc2744e122fa166bf89743a751
Nikhil <nikhofthehil@gmail.com>
2017-12-24 16:08:58 -0600

    Adding templates for nav bar URLs

--- iamshakti/urls.py
+++ iamshakti/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import *
 from . import views
 
-urlpatterns = [
-    url('', views.index, name='index'),
-]
 
-#urlpatterns = patterns('',
-#    url('', views.index, name='index')
-#);
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^initiatives/$', views.initiatives, name='initiatives'),
+    url(r'^stories/$', views.stories, name='stories'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^more/$', views.more, name='more'),
+]
--- iamshakti/views.py
+++ iamshakti/views.py
@@ -2,7 +2,46 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+
 def index(request):
-    template = loader.get_template('base.html')
-    context = { 'yolo': 3, }
+    template = loader.get_template('index.html')
+    context = {
+        None : None,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def about(request):
+    template = loader.get_template('about.html')
+    context = {
+        None : None,
+    }
+    return HttpResponse(template.render(context, request))
+
+def initiatives(request):
+    template = loader.get_template('initiatives.html')
+    context = {
+        None : None,
+    }
+    return HttpResponse(template.render(context, request))
+
+def stories(request):
+    template = loader.get_template('stories.html')
+    context = {
+        None : None,
+    }
+    return HttpResponse(template.render(context, request))
+
+def contact(request):
+    template = loader.get_template('contact.html')
+    context = {
+        None : None,
+    }
+    return HttpResponse(template.render(context, request))
+
+def more(request):
+    template = loader.get_template('more.html')
+    context = {
+        None : None,
+    }
     return HttpResponse(template.render(context, request))
