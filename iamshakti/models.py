9f009d5672b664b2c29d20a7d524040b857df62a
Nikhil <nikhilcharles94@gmail.com>
2018-12-28 13:45:17 -0600

    I AM A DATABASE

--- iamshakti/models.py
+++ iamshakti/models.py
@@ -2,4 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Story(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50, blank=True, null=True)
+    postdate = models.DateField()
+    cwarning = models.CharField(max_length=300, blank=True, null=True)
+    tags = models.CharField(max_length=200, blank=True, null=True)
+    text = models.CharField(max_length=8000, blank=True, null=True)
+    mediaurl = models.CharField(max_length=2000, blank=True, null=True)
+    storyid = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        managed = False
+        db_table = 'stories'
