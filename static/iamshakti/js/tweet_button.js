4a59cd77dc76896105562ee4e040b47031f04ae7
Nikhil <nikhilcharles94@gmail.com>
2019-02-09 14:43:25 -0600

    oooops gotta URL encode the tweet

--- static/iamshakti/js/tweet_button.js
+++ static/iamshakti/js/tweet_button.js
@@ -1,6 +1,6 @@
 function fillTweet() {
     var tweetText = document.getElementById("tweetText").value;
     var tweetButton = document.getElementById("tweetButton");
-    var tweetLink = "https://twitter.com/intent/tweet?hashtags=IAMSHAKTI&text="+tweetText;
+    var tweetLink = "https://twitter.com/intent/tweet?hashtags=IAMSHAKTI&text="+encodeURIComponent(tweetText);
     tweetButton.setAttribute("href", tweetLink);
 }
