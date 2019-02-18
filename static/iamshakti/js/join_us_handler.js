94893f5ab733a6dd7233aac83bd9a5488ee91e19
Nikhil <nikhilcharles94@gmail.com>
2019-02-17 20:19:40 -0600

    Refactoring JS code

--- static/iamshakti/js/join_us_handler.js
+++ static/iamshakti/js/join_us_handler.js
@@ -6,18 +6,10 @@ function error(err) {
     console.log("There was an error :(");
 }
 
-function jtm_member() {
+function send_jtm(data) {
     var xhr = new XMLHttpRequest();
     xhr.onload = success;
     xhr.onerror = error;
-
-    var data = {
-                    "firstName": document.getElementById('firstName').value,
-                    "lastName": document.getElementById('lastName').value,
-                    "email": document.getElementById('emailAddr1').value,
-                    "interests": document.getElementById('interests').value,
-                    "memberType": "ME"
-               }
 
     xhr.open('POST', 'http://127.0.0.1:8000/api/jtm/');
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
@@ -26,19 +18,22 @@ function jtm_member() {
     xhr.send(JSON.stringify(data));
 }
 
-function jtm_gen_ally() {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = success;
-    xhr.onerror = error;
+function jtm_member() {
 
+    var data = {
+                    "firstName": document.getElementById('firstName').value,
+                    "lastName": document.getElementById('lastName').value,
+                    "email": document.getElementById('emailAddr1').value,
+                    "interests": document.getElementById('interests').value,
+                    "memberType": "ME"
+               }
+    send_jtm(data);
+}
+
+function jtm_gen_ally() {
     var data = {
                     "email": document.getElementById('emailAddr2').value,
                     "memberType": "GA"
                }
-
-    xhr.open('POST', 'http://127.0.0.1:8000/api/jtm/');
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
-
-    xhr.send(JSON.stringify(data));
+    send_jtm(data);
 }
