5373c02f7922b1476c3eb095852d7cf11157383d
Nikhil <nikhilcharles94@gmail.com>
2020-02-22 16:45:38 -0600

    updating settings.py

--- iamshakti-proj/settings.py
+++ iamshakti-proj/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aaaa'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [u'localhost', u'127.0.0.1']
+ALLOWED_HOSTS = [u'localhost', u'127.0.0.1', u'0.0.0.0', u'wearesaath.herokuapp.com']
 
 
 # Application definition
