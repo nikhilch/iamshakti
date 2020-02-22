90359b575e5296351b70070878de2670870137d8
Nikhil <nikhilcharles94@gmail.com>
2020-02-22 17:10:22 -0600

    Fixing dj_database_url errors

--- iamshakti-proj/settings.py
+++ iamshakti-proj/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import django_heroku
-import dj-database-url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'iamshakti-proj.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES[‘default’].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 
